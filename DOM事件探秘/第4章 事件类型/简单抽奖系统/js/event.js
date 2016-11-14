fd755f528fea4d9a2659e637964f24400092101c
327134 <825873709@qq.com>
2016-11-14 17:44:59 +0800

    学习DOM事件探秘

--- DOM事件探秘/第4章 事件类型/简单抽奖系统/js/event.js
+++ DOM事件探秘/第4章 事件类型/简单抽奖系统/js/event.js
@@ -3,19 +3,42 @@
  * 事件处理
  */
 
-
+var prize=["iphone7", "佳能相机", "Macbook Pro", "iwatch","优惠券100元", "外星人笔记本"];
+var timer=null;
+var flag = true;
 
 window.onload = function()
 {
     var start = document.getElementById("start");
     var stop = document.getElementById("stop");
 
+    //鼠标事件
     start.onclick = begin;
     stop.onclick = end;
-}
 
-var prize=["iphone7", "佳能相机", "Macbook Pro", "iwatch","优惠券100元", "外星人笔记本"];
-var timer=null;
+    //键盘事件
+    /* 鼠标长按，事件会重复执行
+    * keyDown : 当用户按下键盘上的任意键时触发，而且如果按住不放的话，会重复触发此事件
+    * keyPress ：当用户按下键盘上的字符键时触发，而且如果按住不放的话，会重复触发此事件
+    * keyUp ：当用户释放键盘上的键时触发
+    */
+    document.onkeyup = function(event)
+    {
+        event = event || window.event;
+        //console.log(event.keyCode);
+        if(event.keyCode == 13)
+        {
+            if(flag) {
+                begin();
+                flag = false;
+            } else if(!flag) {
+                end();
+                flag = true;
+            }
+        }
+    }
+
+}
 
 function begin()
 {
@@ -29,6 +52,8 @@ function begin()
     var start = document.getElementById("start");
     start.style.backgroundColor="#999";
     start.onclick = null;
+
+    flag = false;
 }
 
 function end()
@@ -38,4 +63,5 @@ function end()
     var start = document.getElementById("start");
     start.style.backgroundColor="#036";
     start.onclick = begin;
+    flag = true;
 }
